7cb686febc5852331858ab9abb80a60a81fd12a9
bash0C7 <ksb.4038.nullpointer+github@gmail.com>
2026-05-02 16:33:44 +0900

    feat: gate create_swift_makefile on Swift 6.3+
    Consumer gems use SwiftGem::Mkmf.create_swift_makefile from extconf.rb,
    so wire SwiftVersionCheck in there too. The probe is injectable so unit
    tests can simulate older toolchains without touching the host's Swift.

--- lib/swift_gem/mkmf.rb
+++ lib/swift_gem/mkmf.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "mkmf"
+require "swift_gem/swift_version_check"
 
 module SwiftGem
   module Mkmf
@@ -13,7 +14,9 @@ module SwiftGem
       File.expand_path(".build/release", source_dir)
     end
 
-    def self.create_swift_makefile(target, package:, source_dir:, builder: DEFAULT_BUILDER)
+    def self.create_swift_makefile(target, package:, source_dir:, builder: DEFAULT_BUILDER,
+                                   swift_version_probe: SwiftVersionCheck.method(:default_probe))
+      SwiftVersionCheck.call!(prober: swift_version_probe)
       lib_dir = builder.call(package, source_dir)
       $LDFLAGS << " -Wl,-rpath,#{lib_dir} -L#{lib_dir} -l#{package}"
       create_makefile(target)
